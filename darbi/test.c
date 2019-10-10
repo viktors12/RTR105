1ad40ab4b74870f769ffca5a231ecdfccea93ff6
user <Lektro58@Gmail.com>
2019-10-10 15:38:03 +0300

    history101119

---
+++ darbi/test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main() {
+int yesno, number,number1;
+
+
+printf("ievadi skaitli Pidar!\n");    
+scanf("%d", &number);
+
+if (number % 2 == 0){
+	printf("Tavs skaitlis %d ir para skaitlis ", number);
+}
+
+else {
+        printf("Tu esi riktigs pidars\n");
+
+}
+
+}
---
+++ darbi/test2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main() {
+int yesno, number,number1;
+
+
+printf("ievadi skaitli Pidar!\n");    
+scanf("%d", &number);
+printf ("-----------%d \n", number);
+if (number % 2 == 0)
+	printf("Tavs skaitlis %d ir para skaitlis\n ", number);
+
+
+
+else if (number == 32764)
+	printf("Tava mamma ir gejs\n ");
+
+
+else 
+        printf("Tu esi riktigs pidars\n");
+}
